250cfdc43db1df9288c4de1f46ab57709932cfd9
Fasile Endalkachew <fendalkachew@excellerentsolutions.com>
2022-03-07 00:51:17 +0300

    gcReactTest first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import LandingPage from "./Pages/LandingPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid grid-cols-6 gap-4 py-44">
+      <div className="col-start-2 col-span-4">
+        <LandingPage />
+      </div>
     </div>
   );
 }
